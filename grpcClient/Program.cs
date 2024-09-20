3c3f546d7c4715e0b0d02e705edac489173df926
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2024-09-20 11:40:31 +0300

    unary

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using grpcMessageClient;
+
+// var channel =GrpcChannel.ForAddress("http://localhost:5284");
+// var greetClient = new Greeter.GreeterClient(channel);
+
+var channel =GrpcChannel.ForAddress("http://localhost:5284");
+var messageClient = new Message.MessageClient(channel);
+
+MessageResponse response  = await messageClient.SendMessageAsync(new MessageRequest{
+    Message ="Merhaba ",
+    Name    = "Dogan"
+});
+System.Console.WriteLine(response.Message);  
+
+// HelloReply result = await greetClient.SayHelloAsync(new HelloRequest
+// {
+//     Name="Ebubekir'den Selamlar!",
+// });
